b02c3833ea47bf3e143ca7395722ab3d462334b9
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2025-09-10 08:34:01 -0500

    added resume images to the resume page and adjusted it to be responisive for mobile and tablet

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -4,17 +4,23 @@ import "../styles/ResumePage.css"; //* Import CSS for ResumePage
 //* Grabbing the resumes pdf version for download and the jpeg versions for displaying
 export default function ResumePage() {
   const pdfUrl = "/Resume.pdf";
-  // const imageUrl = "/Page1.png";
-  // const imageUrl2 = "/Page2.png";
+  const imageUrl = "/Page1.png";
+  const imageUrl2 = "/Page2.png";
+  const imageUrl3 = "/Page3.png";
+  const imageUrl4 = "/Page4.png";
+  const imageUrl5 = "/Page5.png";
 
   return (
     <div id="resume">
       <div className="container">
         <h1 className="sub-title">My Resume</h1>
-        {/* <div className="image-container">
+        <div className="image-container">
           <img src={imageUrl} alt="Resume" className="resume-image" />
           <img src={imageUrl2} alt="Resume" className="resume-image" />
-        </div> */}
+          <img src={imageUrl3} alt="Resume" className="resume-image" />
+          <img src={imageUrl4} alt="Resume" className="resume-image" />
+          <img src={imageUrl5} alt="Resume" className="resume-image" />
+        </div>
         <a
           href={pdfUrl}
           download="Joshua Garcia's Resume.pdf"
