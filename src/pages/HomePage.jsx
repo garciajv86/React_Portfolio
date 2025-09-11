3895feafb4822cdf91ec9cb2eda043289852c941
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2025-09-11 10:12:29 -0500

    fixing homepage responsiveness issues

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,20 @@
 // import React from "react";
 import "../styles/HomePage.css";
+import mobileBg from "../assets/images/MobileBackground.png";
 
 const HomePage = () => {
   return (
     <div id="header">
       <div className="container">
         <div className="header-text">
-          <p>Full-Stack Web Designer</p>
+          <p>Full-Stack Web Developer</p>
           <h1>
             Hi, I'm <span>Joshua</span>
             <br />
             Garcia From Kansas
           </h1>
         </div>
+        <img src={mobileBg} alt="Background" className="mobile-hero" />
       </div>
     </div>
   );
